93f638434fba64c35186f07a2e6974026a8c872c
Pavel <40184203+PavelSergeevichF@users.noreply.github.com>
2023-06-25 08:54:58 +0300

    23,06,25_8:54

--- Assets/Scripts/Net/RegistrationView.cs
+++ Assets/Scripts/Net/RegistrationView.cs
@@ -2,6 +2,7 @@ using PlayFab;
 using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,7 +13,10 @@ public class RegistrationView : MonoBehaviour
     [SerializeField] private InputField _userPasswordField;
     [SerializeField] private InputField _userCheckPasswordField;
     [SerializeField] private Button _registrationButton;
+    [SerializeField] private Button _authorizatioButton;
+    [SerializeField] private Image _image;
     [SerializeField] private Text _errorText;
+    [SerializeField] private Text _infoText;
     [SerializeField] private SOUserData _sOUserData;
     [SerializeField] private SelectAuthorizationOrRegistrationView _selectAuthorizationOrRegistrationView;
 
@@ -26,7 +30,8 @@ public class RegistrationView : MonoBehaviour
         _userNameField.onValueChanged.AddListener(SetUserName);
         _userEmailField.onValueChanged.AddListener(SetUserEmale);
         _userPasswordField.onValueChanged.AddListener(SetUserPassword);
-        _registrationButton.onClick.AddListener(Submit);
+        _registrationButton.onClick.AddListener(SubmitRegistration);
+        _authorizatioButton.onClick.AddListener(SubmitAuthorization);
         _errorText.text = "";
     }
     private void SetUserName(string value)
@@ -41,7 +46,7 @@ public class RegistrationView : MonoBehaviour
     {
         _userPassword=value;
     }
-    private void Submit()
+    private void SubmitRegistration()
     {
         PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
         {
@@ -63,6 +68,31 @@ public class RegistrationView : MonoBehaviour
             _errorText.gameObject.SetActive(true);
             _errorText.text = error.ErrorMessage;
             Debug.LogError(error);
+        }
+        );
+    }
+    private void SubmitAuthorization()
+    {
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+        {
+            Username = _userName,
+            Password = _userPassword
+        }, result =>
+        {
+            _errorText.gameObject.SetActive(false);
+            Debug.Log($"User registrated: {result.LastLoginTime}");
+            _infoText.text = $"Enter last time {result.LastLoginTime}";
+            _selectAuthorizationOrRegistrationView.SelectAuthorizationOrRegistrationController.ClosePanels();
+            //_image.color = Color.green;
+            _sOUserData.UserName = _userName;
+            _sOUserData.UserPassword = _userPassword;
+
+        }, error =>
+        {
+            _errorText.gameObject.SetActive(true);
+            _errorText.text = error.ErrorDetails.FirstOrDefault().Value.FirstOrDefault() ?? "";
+            Debug.LogError(error);
+            //_image.color = Color.red;
         }
         );
     }
